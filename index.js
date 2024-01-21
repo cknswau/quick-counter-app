61910185eb30e3ed38174f05797a525e73b56b86
Chris Kay <chris@eleet.com.au>
2024-01-21 20:36:58 +1100

    updates

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let incBtn = document.getElementById("increment-btn")
+let saveBtn = document.getElementById("save-btn")
 let count = 0
 
-function increment() {
+incBtn.addEventListener("click", function() {
     count += 1
     countEl.textContent = count
-}
+})
 
-function save() {
+saveBtn.addEventListener("click", function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-}
+})
